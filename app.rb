5294cb26a6e6d17fa6fd722eb57e2dd3e157970e
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-10-06 21:17:28 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,14 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
   get '/' do
     erb :user_input
   end
 
-  post '/ ' do
-      phrase_from_user = params[:user_phrase]
-      @piglatinized = PigLatinizer.new(phrase_from_user)
-      erb :display
-    end
+  post '/piglatinize' do
+    pl = PigLatinizer.new
+    @piglatin = pl.piglatinize(params[:user_phrase])
+    erb :results
+  end
 
 end
