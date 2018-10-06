02feac9c596d27d5a39b8954fd191f520c1a9bc9
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-10-06 21:13:21 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :user_input
+  end
+
+  post '/ ' do
+      phrase_from_user = params[:user_phrase]
+      @piglatinized = PigLatinizer.new(phrase_from_user)
+      erb :display
+    end
+
 end
